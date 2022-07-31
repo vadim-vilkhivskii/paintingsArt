b9ebe19f10bad31315c86247b6ad19ef90f666ec
Vadim Vilkhivskiy <vadim.v.valki@gmail.com>
2022-07-31 21:27:12 +0300

    added functionality for modal windows

---
+++ src/js/modules/sliders.js
@@ -0,0 +1,12 @@
+const sliders = (slides, dir, prev, next) => {
+    let slideIndex = 1;//текущий слайд
+    const items = document.querySelectorAll('slides'),
+    prevBtn = document.querySelector(prev),
+    nextBtn = document.querySelector(next);
+
+    showSlides = (n) => {
+        
+    }
+}
+
+export default sliders;
