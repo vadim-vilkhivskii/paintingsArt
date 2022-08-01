fd7adac144631f0e70e275487ff6171e34e81558
Vadim Vilkhivskiy <vadim.v.valki@gmail.com>
2022-08-01 03:03:07 +0300

    added pagination of images

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import sliders from './modules/sliders';
 import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInput from './modules/checkTextInput';
+import showMoreStyles from './modules/showMoreStyles';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict'
@@ -15,4 +16,5 @@ window.addEventListener('DOMContentLoaded', () => {
     mask('[name="phone"]');
     checkTextInput('[name="name"]');
     checkTextInput('[name="message"]');
+    showMoreStyles('.button-styles', '.styles-2');
 })
